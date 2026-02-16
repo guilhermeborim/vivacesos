eca09d74f2100fe22102c644a850fbba26ea1f61
guilhermeborim <borimdev@gmail.com>
2026-02-16 11:42:24 -0300

    fix: verifica se profissional já está vinculado em uma clínica

--- api/src/domain/professional/services/register.ts
+++ api/src/domain/professional/services/register.ts
@@ -1,20 +1,35 @@
 import { hashSync } from "bcrypt";
 import { Professional } from "../../../infra/database/typeorm/sass/entities/Professional";
 import { CreateProfessionalParams } from "../../../infra/database/typeorm/sass/interfaces/professional";
+import { ClinicUsersTypeormRepository } from "../../../infra/database/typeorm/sass/repositories/clinic-users.repository";
 import { ProfessionalTypeormRepository } from "../../../infra/database/typeorm/sass/repositories/professional.repository";
+import { UnauthenticatedError } from "../../../shared/errors/unauthenticated.error";
 
 export class RegisterProfessionalService {
   private professionalRepository: ProfessionalTypeormRepository;
+  private clinicUserRepository: ClinicUsersTypeormRepository;
 
   constructor() {
     this.professionalRepository = new ProfessionalTypeormRepository();
+    this.clinicUserRepository = new ClinicUsersTypeormRepository();
   }
 
   async execute(
     clinicId: string,
     professional: CreateProfessionalParams,
   ): Promise<Professional> {
-    //TODO: Verificar se o usuário já esta vinculado como professional na clínica.
+    const professionalVinculedInClinic =
+      await this.clinicUserRepository.getUserBindedClinic(
+        clinicId,
+        professional.userId,
+      );
+
+    if (professionalVinculedInClinic) {
+      throw new UnauthenticatedError(
+        "Profissional já vinculado nesta clínica!",
+      );
+    }
+
     if (professional.crm) {
       const crmHash = hashSync(professional.crm, 10);
       professional.crm = crmHash;
